54ca7ffe28f4de8289a00261eb17578ec1d4bc27
Lucas Dota Trombini <119457135+Lucasdota@users.noreply.github.com>
2023-06-10 22:11:56 -0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      "2xl": {max: "1535px"},
+      "xl": {max: "1279px"},
+      "lg": {max: "1023px"},
+      "md": {max: "767px"},
+      "sm": {max: "639px"},
+      "xs": {max: "479px"}
+    },
     extend: {
+      colors: {
+        'coolblue': '#1b99d3'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
